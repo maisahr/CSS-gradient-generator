77601b37989bdf35046ab1994d8a8c0e7bee7654
maisahr <ma.isahr7@gmail.com>
2022-06-17 13:11:19 -0700

    Adds directions buttons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,20 @@
+import { useState } from 'react'
+import { Linear, Radial } from '../Directions/Directions'
 import './Sidebar.css'
+
 export const Sidebar = () => {
+    const [direction, setDirection] = useState('linear')
+
     return (
         <div className="sidebar">
             <h1>CSS GRADIENT GENERATOR</h1>
 
             <h2>Style</h2>
-            <button>Linear</button>
-            <button>Radial</button>
+            <button id='linear' onClick={() => setDirection('linear')}>Linear</button>
+            <button id='radial' onClick={() => setDirection('radial')}>Radial</button>
 
             <h2>Direction</h2>
+            {direction === 'linear' ? <Linear></Linear> : <Radial></Radial>}
 
             <h2>Colors</h2>
             <button>A</button>
