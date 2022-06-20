a58051a5f5fb613b6880649c0e537712217dc6ba
maisahr <ma.isahr7@gmail.com>
2022-06-19 20:53:53 -0700

    Finishes templates layout design

--- src/components/Templates/Templates.js
+++ src/components/Templates/Templates.js
@@ -2,19 +2,21 @@ import './Templates.css'
 
 export const Templates = () => {
     return(
-        <section className="templates">
+        <section className="templatesSection">
             <h2>Templates</h2>
-            <div>
-                <figure id='template1'></figure>
-                <figcaption>"Template" by user</figcaption>
-            </div>
-            <div>
-                <figure id='template2'></figure>
-                <figcaption>"Template" by user</figcaption>
-            </div>
-            <div>
-                <figure id='template3'></figure>
-                <figcaption>"Template" by user</figcaption>
+            <div className='templates'>
+                <div className='template'>
+                    <figure id='template1'></figure>
+                    <figcaption>"Template" by user</figcaption>
+                </div>
+                <div className='template'>
+                    <figure id='template2'></figure>
+                    <figcaption>"Template" by user</figcaption>
+                </div>
+                <div className='template'>
+                    <figure id='template3'></figure>
+                    <figcaption>"Template" by user</figcaption>
+                </div>
             </div>
         </section>
     )
